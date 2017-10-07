d80738995d01374807cdecca6d9249249f526ac2
北海若 <shiruoxi2010@163.com>
2017-10-07 20:44:08 +0800

    win32 project Launcher

---
+++ DanmakuRunner-win32/src/cn/s3bit/danmakurunner/win32/DRunnerContainer.java
@@ -0,0 +1,32 @@
+package cn.s3bit.danmakurunner.win32;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+
+import cn.s3bit.danmakurunner.runner.DRunner;
+
+/**
+ * 
+ * @author 北海若
+ * <p>
+ * Contains a LwjglApplication, which contains a {@link DRunner}.
+ * </p>
+ */
+public class DRunnerContainer {
+	/**
+	 * The {@link LwjglApplication}, an OpenGL surface full-screen or in a lightweight window.
+	 */
+	public final LwjglApplication application;
+	
+	private DRunnerContainer() {
+		application = new LwjglApplication(new DRunner(), "Danmaku Runner - Playback", 540, 960);
+	}
+	
+	/**
+	 * Factory method.
+	 * @return a new {@link DRunnerContainer}
+	 */
+	public static DRunnerContainer instanciate() {
+		DRunnerContainer instance = new DRunnerContainer();
+		return instance;
+	}
+}
---
+++ DanmakuRunner-win32/src/cn/s3bit/danmakurunner/win32/Launcher.java
@@ -0,0 +1,7 @@
+package cn.s3bit.danmakurunner.win32;
+
+public class Launcher {
+	public static void main(String[] args) {
+		DRunnerContainer.instanciate();
+	}
+}
